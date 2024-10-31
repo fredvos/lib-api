fc30808a2896dd9f7d20bec9a161f6c95e324555
Fred Vos <fred.vos@tilburguniversity.edu>
2024-10-31 20:13:24 +0100

    Removes pydantic_settings library from dependencies, since these are not available as a Debian package. Use dotenv.

--- src/config.py
+++ src/config.py
@@ -1,13 +1,29 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from dotenv import dotenv_values
+from pydantic import BaseModel
 
 
-class Settings(BaseSettings):
+class Settings(BaseModel):
     """
     This class defines the settings for the app
     """
-    POSTGRES_URL: str
+    postgres_host: str
+    postgres_user: str
+    postgres_password: str
+    postgres_database: str
+    postgres_schema: str
+    
+    def get_postgres_url(self) ->  str:
+        return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}/{self.postgres_database}"
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
+envdict = dotenv_values(".env")
+print("envdict =", envdict)
+settings = Settings(
+    postgres_host=str(envdict.get("POSTGRES_HOST")),
+    postgres_user=str(envdict.get("POSTGRES_USER")),
+    postgres_password=str(envdict.get("POSTGRES_PASSWORD")),
+    postgres_database=str(envdict.get("POSTGRES_DATABASE")),
+    postgres_schema=str(envdict.get("POSTGRES_SCHEMA"))
+)
 
-settings = Settings()
+print("postgres_url =", settings.get_postgres_url())
